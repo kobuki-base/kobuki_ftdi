b6f530c00257f85e2ec30d800b620dabfbd0a4f5
Daniel Stonier <d.stonier@gmail.com>
2020-01-12 15:18:28 -0500

    [read] check return results from ftdi_read_eeprom (#2)

--- src/ftdi_kobuki.cpp
+++ src/ftdi_kobuki.cpp
@@ -122,6 +122,11 @@ int main(int argc, char **argv)
   ftdi_eeprom eeprom;
   unsigned char eeprom_binary[512];
   int result = ftdi_read_eeprom(&ftdi,eeprom_binary);
+  if (result < 0)
+  {
+    std::cerr << "Failed to read the eeprom from the requested device." << std::endl;
+    return -1;
+  }
   int size = FTDI_DEFAULT_EEPROM_SIZE;
   // this never works for me
 //  int size = ftdi_read_eeprom_getsize(&ftdi, eeprom_binary, 512);
